f2d1cb3fc2e1128fa152c1b11572ad30eb950f60
btbrq <brqcode@gmail.com>
2022-03-29 20:29:32 +0200

    - Remove effects from input. - Check for metaKey & altKey when opening url.

--- src/util/open-url.js
+++ src/util/open-url.js
@@ -1,7 +1,7 @@
 /*global chrome*/
 export const openUrl = (e, url) => {
     window.close();
-    if (!e.ctrlKey && !e.shiftKey) {
+    if (!e.ctrlKey && !e.shiftKey && !e.metaKey && !e.altKey) {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             var tab = tabs[0];
             chrome.tabs.update(tab.id, { url: url });
